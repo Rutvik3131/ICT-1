7bc37f10fb6cc52d45d1dba0c0ab8c4fb0a03460
Rutvik3131 <34470994+Rutvik3131@users.noreply.github.com>
2018-09-25 20:32:22 +1000

    update
    profile

--- resources/views/pages/profile page.blade.php
+++ resources/views/pages/profile page.blade.php
@@ -16,11 +16,12 @@
 
         <!--Google Fonts-->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800|Raleway:400,500,700|Roboto:300,400,500,700,900|Ubuntu:300,300i,400,400i,500,500i,700" rel="stylesheet">
-        <!-- Main CSS -->
-        <link rel="stylesheet" href="css/style.css" />
         <!-- Responsive CSS -->
         <link rel="stylesheet" href="css/responsive.css" />
         <link rel="stylesheet" href="css/profile page.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -188,44 +189,8 @@
                 </div>
             </div><!-- // row -->
         </div><!-- // container-full -->
-      </header><!-- // header -->
-
-      <div id="main-category">
-        <div class="container-full">
-        	<div class="row">
-                <div class="col-md-12">
-                    <ul class="main-category-menu">
-                        <li class="color-1"><a href="02-category.html"><i class="fa fa-music"></i>Music</a></li>
-                        <li class="color-2"><a href="02-category.html"><i class="fa fa-soccer-ball-o"></i>Sports</a></li>
-                        <li class="color-3"><a href="02-category.html"><i class="fa fa-gamepad"></i>Gaming</a></li>
-                        <li class="color-4"><a href="02-category.html"><i class="fa fa-globe"></i>News</a></li>
-                        <li class="color-1"><a href="02-category.html"><i class="fa fa-play-circle-o"></i>Live</a></li>
-                        <li class="color-2"><a href="02-category.html"><i class="fa fa-repeat"></i>360° Video</a></li>
-                        <li class="color-3"><a href="02-category.html"><i class="fa fa-th-large"></i>Template Pages</a>
-                        	<ul>
-                            	<li><a href="01-home.html">01-home </a></li>
-                            	<li><a href="02-category.html">02-category </a></li>
-                            	<li><a href="03-category_videos.html">03-category_videos  </a></li>
-                            	<li><a href="04-category_chanels.html">04-category_chanels </a></li>
-                            	<li><a href="05-category_playlists.html">05-category_playlists </a></li>
-                            	<li><a href="06-category_about.html">06-category_about </a></li>
-                            	<li><a href="07-log_in_page.html">07-log_in_page </a></li>
-                            	<li><a href="08-sign_up_page.html">08-sign_up_page </a></li>
-                            	<li><a href="09-watch.html">09-watch </a></li>
-                            	<li><a href="10-upload.html">10-upload </a></li>
-                            	<li><a href="11-blog.html">11-blog </a></li>
-                            	<li><a href="12-single.html">12-single </a></li>
-                            	<li><a href="13-404_page.html">13-404_page </a></li>
-                            	<li><a href="14-history.html">14-history </a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div><!-- // col-md-14 -->
-              </div><!-- // row -->
-          </div><!-- // container-full -->
-      </div><!-- // main-category -->
-
-       <div class="site-output">
+      
+        <div class="site-output">
       	<div class="col-md-2 no-padding-left hidden-sm hidden-xs">
         	<div class="left-sidebar">
             	<div id="sidebar-stick" >
@@ -249,95 +214,104 @@
                 <div class="clear"></div>
             </div><!-- // left-sidebar -->
         </div><!-- // col-md-2 -->
- 
 
+     
+        <body class="profile-page">
+    <div class="page-header header-filter" data-parallax="true" style="background-image:url('http://wallpapere.org/wp-content/uploads/2012/02/black-and-white-city-night.png');"></div>
+    <div class="pmain pmain-raised">
+		<div class="profile-content">
+            <div class="container">
+                <div class="row">
+	                <div class="col-md-6 ml-auto mr-auto">
+        	           <div class="profile">
+	                        <div class="avatar">
+	                            <img src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU0NjQzOTk4OTQ4OTkyMzQy/ansel-elgort-poses-for-a-portrait-during-the-baby-driver-premiere-2017-sxsw-conference-and-festivals-on-march-11-2017-in-austin-texas-photo-by-matt-winkelmeyer_getty-imagesfor-sxsw-square.jpg" alt="Circle Image" class="img-raised rounded-circle img-fluid">
+	                        </div>
+	                        <div class="name">
+	                            <h3 class="title">Christian Louboutin</h3>
+								<h6>Designer</h6>
+								<a href="#pablo" class="btn btn-just-icon btn-link btn-dribbble"><i class="fa fa-dribbble"></i></a>
+                                <a href="#pablo" class="btn btn-just-icon btn-link btn-twitter"><i class="fa fa-twitter"></i></a>
+                                <a href="#pablo" class="btn btn-just-icon btn-link btn-pinterest"><i class="fa fa-pinterest"></i></a>
+	                        </div>
+	                    </div>
+    	            </div>
+                </div>
+                <div class="description text-center">
+                    <p>An artist of considerable range, Chet Faker — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. </p>
+                </div>
+				<div class="row">
+					<div class="col-md-6 ml-auto mr-auto">
+                        <div class="profile-tabs">
+                          <ul class="nav nav-pills nav-pills-icons justify-content-center" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#studio" role="tab" data-toggle="tab">
+                                  <i class="material-icons">camera</i>
+                                  Studio
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#works" role="tab" data-toggle="tab">
+                                  <i class="material-icons">palette</i>
+                                    Work
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#favorite" role="tab" data-toggle="tab">
+                                  <i class="material-icons">favorite</i>
+                                    Favorite
+                                </a>
+                            </li>
+                          </ul>
+                        </div>
+    	    	</div>
+            </div>
+        
+          <div class="tab-content tab-space">
+            <div class="tab-pane active text-center gallery" id="studio">
+  				<div class="row">
+  					<div class="col-md-3 ml-auto">
+  					    <img src="https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83079913579babb9d2c94a5941b2e69d&auto=format&fit=crop&w=751&q=80" class="rounded">
+  						<img src="https://images.unsplash.com/photo-1528249227670-9ba48616014f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=66b8e7db17b83084f16fdeadfc93b95b&auto=format&fit=crop&w=357&q=80" class="rounded">
+  					</div>
+  					<div class="col-md-3 mr-auto">
+  						<img src="https://images.unsplash.com/photo-1521341057461-6eb5f40b07ab?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=72da2f550f8cbd0ec252ad6fb89c96b2&auto=format&fit=crop&w=334&q=80" class="rounded">
+  						<img src="https://images.unsplash.com/photo-1506667527953-22eca67dd919?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6326214b7ce18d74dde5e88db4a12dd5&auto=format&fit=crop&w=750&q=80" class="rounded">
+  					</div>
+  				</div>
+  			</div>
+            <div class="tab-pane text-center gallery" id="works">
+      			<div class="row">
+      				<div class="col-md-3 ml-auto">
+                      <img src="https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83079913579babb9d2c94a5941b2e69d&auto=format&fit=crop&w=751&q=80" class="rounded">
+  					  <img src="https://images.unsplash.com/photo-1506667527953-22eca67dd919?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6326214b7ce18d74dde5e88db4a12dd5&auto=format&fit=crop&w=750&q=80" class="rounded">
+  					  <img src="https://images.unsplash.com/photo-1505784045224-1247b2b29cf3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec2bdc92a9687b6af5089b335691830e&auto=format&fit=crop&w=750&q=80" class="rounded">  					</div>
+      				<div class="col-md-3 mr-auto">
+                      <img src="https://images.unsplash.com/photo-1504346466600-714572c4b726?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6754ded479383b7e3144de310fa88277&auto=format&fit=crop&w=750&q=80" class="rounded">
+                      <img src="https://images.unsplash.com/photo-1494028698538-2cd52a400b17?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83bf0e71786922a80c420c17b664a1f5&auto=format&fit=crop&w=334&q=80" class="rounded">
+      				</div>
+      			</div>
+  			</div>
+            <div class="tab-pane text-center gallery" id="favorite">
+      			<div class="row">
+      				<div class="col-md-3 ml-auto">
+      				  <img src="https://images.unsplash.com/photo-1504346466600-714572c4b726?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6754ded479383b7e3144de310fa88277&auto=format&fit=crop&w=750&q=80" class="rounded">
+                      <img src="https://images.unsplash.com/photo-1494028698538-2cd52a400b17?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83bf0e71786922a80c420c17b664a1f5&auto=format&fit=crop&w=334&q=80" class="rounded">
+      				</div>
+      				<div class="col-md-3 mr-auto">
+      				  <img src="https://images.unsplash.com/photo-1505784045224-1247b2b29cf3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec2bdc92a9687b6af5089b335691830e&auto=format&fit=crop&w=750&q=80" class="rounded">  					
+      				  <img src="https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83079913579babb9d2c94a5941b2e69d&auto=format&fit=crop&w=751&q=80" class="rounded">
+  					  <img src="https://images.unsplash.com/photo-1506667527953-22eca67dd919?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6326214b7ce18d74dde5e88db4a12dd5&auto=format&fit=crop&w=750&q=80" class="rounded">
+      			    </div>
+      			</div>
+      		</div>
+          </div>
 
-
-      <div class="col-lg-10 col-sm-8">
-    <div class="card hovercard">
-        <div class="card-background">
-            <img class="card-bkimg" alt="" src="http://lorempixel.com/100/100/people/9/">
-            <!-- http://lorempixel.com/850/280/people/9/ -->
+        
+            </div>
         </div>
-        <div class="useravatar">
-            <img alt="" src="http://lorempixel.com/100/100/people/9/">
-        </div>
-        <div class="card-info"> <span class="card-title">Pamela Anderson</span>
-
-        </div>
-    </div>
-    <div class="btn-pref btn-group btn-group-justified btn-group-lg" role="group" aria-label="...">
-        <div class="btn-group" role="group">
-            <button type="button" id="stars" class="btn btn-primary" href="#tab1" data-toggle="tab"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-                <div class="hidden-xs">About ME</div>
-            </button>
-            
-        </div>
-        <div class="btn-group" role="group">
-            <button type="button" id="favorites" class="btn btn-default" href="#tab2" data-toggle="tab"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-                <div class="hidden-xs">Posts</div>
-            </button>
-        </div>
-        <div class="btn-group" role="group">
-            <button type="button" id="following" class="btn btn-default" href="#tab3" data-toggle="tab"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                <div class="hidden-xs">Subscribe</div>
-            </button>
-        </div>
-    </div>
-
-        <div class="well">
-      <div class="tab-content">
-        <div class="tab-pane fade in active" id="tab1">  
-        </div>
-        <div class="tab-pane fade in" id="tab2">
-        <div class="videoItem">
-        <p class="category">Category</p>
-        <a href="#" class="thumbnail">
-          <span class="duration">05:00</span>
-          <img src="https://s1.dmcdn.net/IDEz8.jpg" />
-          <div class="play"></div>
-        </a>
-        <p class="title"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam massa dui, ornare eu porttitor ut, tristique ac tellus.</a></p>
-        <p class="author"><a href="#">Author</a></p>
-      </div>
-      <div class="videoItem">
-        <p class="category">Category</p>
-        <a href="#" class="thumbnail">
-          <span class="duration">05:00</span>
-          <img src="https://s1.dmcdn.net/IDEz8.jpg" />
-          <div class="play"></div>
-        </a>
-        <p class="title"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam massa dui, ornare eu porttitor ut, tristique ac tellus.</a></p>
-        <p class="author"><a href="#">Author</a></p>
-      </div><div class="videoItem">
-        <p class="category">Category</p>
-        <a href="#" class="thumbnail">
-          <span class="duration">05:00</span>
-          <img src="https://s1.dmcdn.net/IDEz8.jpg" />
-          <div class="play"></div>
-        </a>
-        <p class="title"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam massa dui, ornare eu porttitor ut, tristique ac tellus.</a></p>
-        <p class="author"><a href="#">Author</a></p>
-      </div><div class="videoItem">
-        <p class="category">Category</p>
-        <a href="#" class="thumbnail">
-          <span class="duration">05:00</span>
-          <img src="https://s1.dmcdn.net/IDEz8.jpg" />
-          <div class="play"></div>
-        </a>
-        <p class="title"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam massa dui, ornare eu porttitor ut, tristique ac tellus.</a></p>
-        <p class="author"><a href="#">Author</a></p>
-      </div>
-</div> 
-       
-        </div>
-        <div class="tab-pane fade in" id="tab3">
-                  </div>
-      </div>
-    </div>
-    
-    </div>
-            
+	</div>
+     
     
            </div>    
         </div>
